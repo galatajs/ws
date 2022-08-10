135f2c466cf5ed6d9024c71a6c191519656c24c8
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-08-10 20:01:58 +0300

    ws: global middleware wrapper created

--- lib/hooks/middleware.hooks.ts
+++ lib/hooks/middleware.hooks.ts
@@ -3,13 +3,26 @@ import {
   GlobalMiddleware,
   MiddlewareImplementer,
 } from "../middleware/global.middleware";
+import { NextFunction, Socket } from "../types/types";
+
+const wrapGlobalMiddleware = (
+  middleware: GlobalMiddleware
+): GlobalMiddleware => {
+  return async (socket: Socket, next: NextFunction): Promise<void> => {
+    try {
+      await middleware(socket, next);
+    } catch (e: any) {
+      next(e);
+    }
+  };
+};
 
 export const createMiddlewareImplementer = (
   middlewares: UniqueSet<GlobalMiddleware>
 ): MiddlewareImplementer => {
   return {
     use(middleware: GlobalMiddleware) {
-      middlewares.add(middleware);
+      middlewares.add(wrapGlobalMiddleware(middleware));
     },
   };
 };
